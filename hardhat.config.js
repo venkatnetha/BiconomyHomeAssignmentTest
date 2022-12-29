721118e91cde96bf95dd648444ae43953735fdd7
VENKATESH KANCHANPALLY <38311122+venkatnetha@users.noreply.github.com>
2022-12-29 16:53:59 +0530

    Update hardhat.config.js

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,9 +1,10 @@
 require("@nomicfoundation/hardhat-toolbox");
+require("dotenv").config();
 
-const INFURA_API_KEY  = "c2f8ade529cb4d60b12c58c4caa73c89"
+const INFURA_API_KEY  = process.env.INFURA_API_KEY;
 
-const GOERLI_PRIVATE_KEY = "e1a863ecced8da882aad4d4801520fea95946c626f35cb591b4af4d4af37a7f7"
-const GOERLI_PRIVATE_KEY_TOKEN = "1926650ea869835e5d02d4717835747e99e6f081719ef6b0f187ee00c7a3fbf4"
+const GOERLI_PRIVATE_KEY = process.env.GOERLI_PRIVATE_KEY;
+const GOERLI_PRIVATE_KEY_TOKEN = process.env.GOERLI_PRIVATE_KEY_TOKEN;
 
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
